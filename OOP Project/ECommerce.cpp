5419f3151a17723152bf7b85fd24dd49fe776ab6
Muaz Khalid <itzmajo786@gmail.com>
2023-07-06 10:54:36 -0700

    Add files via upload
    Create a Login and Registration  for the users also include file handling for saving the data of the users

--- OOP Project/ECommerce.cpp
+++ OOP Project/ECommerce.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <stdlib.h>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
 class Person
 {
@@ -24,6 +25,10 @@ public:
     {
         return Email;
     }
+    string getname()
+    {
+        return Name;
+    }
     string getpass()
     {
         return Pass;
@@ -42,8 +47,55 @@ public:
 
 int main()
 {
+    fstream userfile("User.txt",ios::app);
+    if (!userfile.is_open())
+    {
+        cout << "Failed to open file!" << endl;
+        return 1;
+    }
     cout << "----------------Menu----------------\n";
+failed_case:
+    bool login = false;
     cout << "1-Login\n";
     cout << "2-Register\n";
+    cout << "Enter Choice: ";
+    int ch;
+    string name, email, pass;
+    string name1, email1, pass1;
+    cin >> ch;
+    Person p1;
     system("cls");
+    if (ch == 1)
+    {
+        cout << "Enter Email:";
+        cin >> email;
+        cout << "Enter Pass:";
+        cin >> pass;
+        bool loginSuccessful = false;
+        while (userfile >> name1 >> email1 >> pass1)
+        {
+            if (email == email1 && pass == pass1)
+            {
+                cout << "Login Successful\n";
+                loginSuccessful = true;
+                break;
+            }
+        }
+
+        if (!loginSuccessful)
+        {
+            cout << "Login Failed" << endl;
+            goto failed_case;
+        }
+    }
+    else if (ch == 2)
+    {
+        cin >> p1;
+        userfile << p1.getname() << " ";
+        userfile << p1.getemail() << " ";
+        userfile << p1.getpass() << endl;
+    }
+
+    userfile.close();
+    return 0;
 }
