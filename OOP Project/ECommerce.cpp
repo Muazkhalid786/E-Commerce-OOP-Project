e0409f342e09eba237002f20920170b8052997f7
Muaz Khalid <itzmajo786@gmail.com>
2023-07-06 10:04:08 -0700

    Add files via upload
    create a class for the users

--- OOP Project/ECommerce.cpp
+++ OOP Project/ECommerce.cpp
@@ -1,1 +1,49 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <stdlib.h>
+using namespace std;
+class Person
+{
+private:
+    string Pass;
+    string Email;
+    string Name;
+
+public:
+    Person()
+    {
+    }
+    Person(string name, string pass, string email)
+    {
+        Pass = pass;
+        Email = email;
+        Name = name;
+    }
+    string getemail()
+    {
+        return Email;
+    }
+    string getpass()
+    {
+        return Pass;
+    }
+    friend istream &operator>>(istream &in, Person &obj)
+    {
+        cout << "Enter Name: ";
+        in >> obj.Name;
+        cout << "Enter Email: ";
+        in >> obj.Email;
+        cout << "Enter Password: ";
+        in >> obj.Pass;
+        return in;
+    }
+};
+
+int main()
+{
+    cout << "----------------Menu----------------\n";
+    cout << "1-Login\n";
+    cout << "2-Register\n";
+    system("cls");
+}
